c24aca584c974a8999030e3299b0f378c4ab14bf
Andrew McMillan <andrew@norramark.com>
2014-10-31 18:39:11 -0400

    laying out the aesthetic groundwork

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	#add a current class if the section is the current section
+	def cp(path)
+		"current" if request.url.include?(path)
+	end
+
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,5 +2,6 @@ class Vendor
   include Mongoid::Document
   field :name, type: String
   field :address, type: String
+  field :contact, type: String
   field :telephone, type: String
 end
