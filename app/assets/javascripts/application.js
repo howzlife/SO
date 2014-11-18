7d3573f355ed4a920f6361fbed75d0e2bbd5aa86
Andrew McMillan <andrew@norramark.com>
2014-11-17 20:21:07 -0500

    live purchase order search, aesthetic changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,8 +44,8 @@ $(function() {
     var searchTimeoutThrottle = 500;
     var searchTimeoutID = -1;
 	$('.search-area .search-query').bind('keyup change', function(){
-        //Only search if search string longer than 2, and it has changed
-        if($(this).val().length > 2 && $(this).val() != $(this).data('oldval')) {
+        // only search if search string has changed
+        if($(this).val() != $(this).data('oldval')) {
         	$(this).data('oldval', $(this).val());
         	if(currReqObj != null) currReqObj.abort();
         	 clearTimeout(searchTimeoutID);
@@ -54,11 +54,18 @@ $(function() {
 				 currReqObj = $.get( "/purchase_orders.json?q=" + term, function( data ) {
 					currReqObj = null;
 					if(data.length == 0) {
+						$('#purchase-orders-table').hide();
+						if (!$('#purchase-orders-search').length) {
+							$('#purchase-orders-table').parent().append('<div id="purchase-orders-search"><h3>Could not find any purchase orders</h3><p>Try changing the search term</p></div>');
+						}
+						$('#purchase-orders-search').show();
 					} else {
+						$('#purchase-orders-table').show();
+						$('#purchase-orders-search').hide();
 						$('#purchase-orders-table tbody').empty();
 						$.each(data, function(index, item) {
 							var url = item.url.slice(0, -5);
-							$('#purchase-orders-table tbody').append('<tr><td><a href="' + url + '">' + item.number + '</a></td><td>' +  $.format.date(item.date, "MMMM D, yyyy") + '</td><td>' + item.vendor.name + '</td><td>' + item.status + '</td></tr>');
+							$('#purchase-orders-table tbody').append('<tr><td><a href="' + url + '">' + item.number + '</a></td><td>' +  $.format.date(item.date, "MMMM D, yyyy") + '</td><td>' + item.vendor.name + '</td><td><span class="label status-' + item.status + '">' + item.status + '</span></td></tr>');
 						});
 						$('.sortable').trigger("update");
 					}
