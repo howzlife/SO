58418601aa14f46fb7be9b182742a16ec2d7f025
Andrew McMillan <andrew@norramark.com>
2014-11-22 14:53:47 -0500

    po changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :companies, only: [:show, :edit, :update]
 
   devise_for :users, controllers: { sessions: 'users/sessions',  registrations: 'users/registrations' }
-  resources :purchase_orders, only: [:index, :show, :new, :create, :destroy]
+  resources :purchase_orders, only: [:index, :show, :new, :create, :update]
 
   resources :vendors
   resources :addresses
