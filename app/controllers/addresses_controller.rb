e080578a3ee81efdc1ab926a83e2b6ee51449e68
Andrew McMillan <andrew@norramark.com>
2014-11-23 17:31:40 -0500

    address changes

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -13,7 +13,7 @@ class AddressesController < ApplicationController
 
     respond_to do |format|
       if @address.save
-        format.html { redirect_to @company, notice: 'address was successfully created.' }
+        format.html { redirect_to @company, notice: 'Address was successfully created.' }
         format.json { render :show, status: :created, location: @address }
       else
         format.html { render :new }
@@ -31,7 +31,7 @@ class AddressesController < ApplicationController
   def update
     respond_to do |format|
       if @address.update(address_params)
-        format.html { redirect_to @company, notice: 'address was successfully updated.' }
+        format.html { redirect_to @company, notice: 'Address was successfully updated.' }
         format.json { render :show, status: :ok, location: @address }
       else
         format.html { render :edit }
@@ -45,7 +45,7 @@ class AddressesController < ApplicationController
   def destroy
     @address.destroy
     respond_to do |format|
-      format.html { redirect_to @company, notice: 'address was successfully destroyed.' }
+      format.html { redirect_to @company, notice: 'Address was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
