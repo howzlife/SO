6e2de0ec5416af6697b0d397c8142e5d8736057f
Andrew McMillan <andrew@norramark.com>
2014-11-27 17:49:02 -0500

    navigation changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 
 	#add a current class if the section is the current section
 	def cp(path)
-		"current" if request.url.include?(path)
+		"current" if request.url.include?(path) || (request.url.include?('addresses') && path == company_path(@company) )
 	end
 
 	#add a current class if the search path is the current path
 	def sp(path)
-		"current" if request.url.include?(path)
+		"current" if request.url.include?(path) || (!params.has_key?(:status) && path == "all" && !params.has_key?(:label))
 	end
 
 end
