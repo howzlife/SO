bd4d63e2e5fff16cf0a5dcdf6e65ce7ba01dba59
howzlife <nico_dubus@hotmail.com>
2015-01-26 22:19:45 -0500

    compiled assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   #Ensure you have defined default url options in your environments files for devise gem
   config.action_mailer.default_url_options = { host: 'powerful-chamber-1551.herokuapp.com' }
-  config.action_mailer.default_options = {  :from => "nico.dubus17@gmail.com" }
+  config.action_mailer.default_options = {  :from => 'nico.dubus17@gmail.com' }
   config.action_mailer.smtp_settings = { 
     address:              'smtp.sendgrid.net',
     port:                 587,
