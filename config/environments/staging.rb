c9b386a551218aa91f80702c2d50458769ab6381
howzlife <nico_dubus@hotmail.com>
2015-01-26 21:00:43 -0500

    testing staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -60,16 +60,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   #Ensure you have defined default url options in your environments files for devise gem
-  config.action_mailer.default_url_options = { host: 'powerful-chamber-1551.herokuapp.com', port: 80 }
-  config.action_mailer.default_options = {  :from => "no-reply@swiftorders.com" }
+  config.action_mailer.default_url_options = { host: 'powerful-chamber-1551.herokuapp.com' }
+  config.action_mailer.default_options = {  :from => "nico.dubus17@gmail.com" }
   config.action_mailer.smtp_settings = { 
     address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'swiftorders.com',
+    domain:               'heroku.com',
     user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
-    enable_starttls_auto: true  
+    enable_starttls_auto: true               
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
