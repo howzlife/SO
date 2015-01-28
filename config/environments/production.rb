d449c35e4a9964acb9957edc061b8397e9de4055
howzlife <nico_dubus@hotmail.com>
2015-01-28 00:27:35 -0500

    deploying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,14 +60,27 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   #Ensure you have defined default url options in your environments files for devise gem
-  config.action_mailer.default_url_options = { host: 'powerful-chamber-1551.com'}
-  config.action_mailer.default_options = {  :from => "no-reply@swiftorders.com" }
+  #config.action_mailer.default_url_options = { host: 'swiftorders.com', port: 80 }
+  #config.action_mailer.default_options = {  :from => "no-reply@swiftorders.com" }
+  #config.action_mailer.smtp_settings = { 
+  #  address:              'smtp.sendgrid.net',
+  #  port:                 587,
+  #  domain:               'swiftorders.com',
+  #  user_name:            ENV['SENDGRID_USERNAME'],
+  #  password:             ENV['SENDGRID_PASSWORD'],
+  #  authentication:       'plain',
+  #  enable_starttls_auto: true  
+  #}
+
+  #FOR PRODUCTION ENVIRONMENT, USE CREDENTIALS BELOW: 
+  config.action_mailer.default_url_options = { host: 'powerful-chamber-1551.herokuapp.com' }
+  config.action_mailer.default_options = {  :from => "no-reply@test.com" }
   config.action_mailer.smtp_settings = { 
     address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'heroku.com',
-    user_name:            'app33286348@heroku.com',#ENV['SENDGRID_USERNAME'],
-    password:             'ch54onoc',#ENV['SENDGRID_PASSWORD'],
+    domain:               'powerful-chamber-1551.heroku.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  
   }
