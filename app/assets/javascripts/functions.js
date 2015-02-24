08d2a4169aabec8e406b375ea32c826f72deea7c
Andrew McMillan <andrew@norramark.com>
2015-02-24 09:43:49 -0500

    hide sidebar search on click outside

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -184,3 +184,14 @@ $(function() {
     }).attr('autocomplete', 'off').data('oldval', '');
     
 });
+
+$(document).mouseup(function (e)
+{
+    var container = $("#results-popup");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.hide();
+    }
+});
